19141c4bce4d5d61899e85d9728a57b5a6f03fe5
NICOLAS <nico.kufeke@gmail.com>
2021-03-25 11:36:44 -0300

    Se crea POJOs y Remote(link con FakeApi y cliente Retrofit

--- app/src/main/java/cl/eme/product2/model/Remote.kt
+++ app/src/main/java/cl/eme/product2/model/Remote.kt
@@ -1,6 +1,8 @@
 package cl.eme.product2.model
 
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -12,4 +14,22 @@ interface Product2API {
 
     @GET ("details/{pid")
     suspend fun getProductDetail2(@Path("pid") id: Int)
+}
+
+//Cliente Retrofit
+
+const val BASE_URL = "https://my-json-server.typicode.com/Himuravidal/FakeAPIdata/"
+
+class RetrofitClient {
+    companion object {
+
+        fun instance() : Product2API{
+            val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+                GsonConverterFactory.create()).build()
+
+            return retrofit.create(Product2API::class.java)
+
+        }
+    }
+
 }
